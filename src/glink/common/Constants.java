64cef6c6aeaf75f32566a0c63846579c207d54c1
dstrassler <daniel.strassler@gmail.com>
2012-08-31 19:37:53 -0400

    --  Added some preliminary information to the Constants class. --  Added commentes to the GridEngineDelegate class.

--- src/glink/common/Constants.java
+++ src/glink/common/Constants.java
@@ -1,9 +1,32 @@
 package glink.common;
 
+//  Using the Constant Data Manager design pattern, store all immutable centrally accessible information. 
 public class Constants {
 
+	//  Class denoting all grid engine types.
 	public static class GridEngineTypes {
-		public static final String SGE = "Sun Grid Engine";
+		
+		//  Class for Sun Grid Engine(prior to acquisition by Oracle).
+		public static class SGE {
+			
+			public static final String TYPE = "Sun Grid Engine";
+			
+			//  Class for Sun Grid Engine accounting file.
+			public static class AccountingFile {
+				
+				//  Class denoting the information of the various fields in the accounting file.
+				public static class Fields {
+					
+					public static class JobId {
+					
+						public static final String NAME = "Job ID";
+						public static final int POSITION = 0;
+					}
+				}
+			}
+		}
+		
+		//  Class for Oracle Grid Engine.
 		public static final String OGE = "Oracle Grid Engine";
 	}
 	
--- src/glink/interfaces/GridEngineDelegate.java
+++ src/glink/interfaces/GridEngineDelegate.java
@@ -1,5 +1,6 @@
 package glink.interfaces;
 
+//  Interface to be implemented for all grid engine types.
 public interface GridEngineDelegate {
 
 }
